1878267af335abe413d99d758c6b4ddcbfe54048
Luan Uchoa <luan_uchoa@outlook.com>
2021-06-25 10:07:40 -0400

    added optional parameter in View for controlling template injection

--- app/ts/views/View.ts
+++ app/ts/views/View.ts
@@ -1,15 +1,23 @@
 export abstract class View<T> {
 
     protected _elemento: JQuery;
+    private _escapar: boolean;
 
-    constructor(seletor: string) {
+   // tornando  o parâmetro opcional!
+    constructor(seletor: string, escapar?: boolean) {
 
         this._elemento = $(seletor);
+        this._escapar = escapar;
     }
+  // código posterior omitido
 
     update(model: T) {
 
-        this._elemento.html(this.template(model));
+        let template = this.template(model);
+        if(this._escapar) 
+            template = template.replace(/<script>[\s\S]*?<\/script>/, '');
+
+        this._elemento.html(template);
     }
 
     abstract template(model: T): string;
