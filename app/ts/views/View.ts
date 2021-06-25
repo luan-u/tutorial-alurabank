43065f6b4d1dcec3ace72092b9620cc585ca982d
Luan Uchoa <luan_uchoa@outlook.com>
2021-06-25 10:49:35 -0400

    apply decorator in View for performance logging

--- app/ts/views/View.ts
+++ app/ts/views/View.ts
@@ -1,29 +1,23 @@
+import { logarTempoDeExecucao } from '../helpers/decorators/index';
+
 export abstract class View<T> {
 
     protected _elemento: JQuery;
     private _escapar: boolean;
 
-   // tirou o tipo opcional que recebia undefined caso não fosse passado para um valor padrão, no caso false
     constructor(seletor: string, escapar: boolean = false) {
 
-        console.log(escape);
         this._elemento = $(seletor);
         this._escapar = escapar;
     }
-  // código posterior omitido
 
+    @logarTempoDeExecucao()
     update(model: T) {
 
-        const t1 = performance.now();
-
         let template = this.template(model);
-        if(this._escapar) 
-            template = template.replace(/<script>[\s\S]*?<\/script>/, '');
-
+        if(this._escapar)
+            template = template.replace(/<script>[\s\S]*?<\/script>/g, '');
         this._elemento.html(template);
-
-        const t2 = performance.now();
-        console.log(`Tempo de execução do método adiciona(): ${(t2 - t1)/1000} segundos`);
 
     }
 
