0b475740b887c33d10295491a177a81c0834ff88
Luan Uchoa <luan_uchoa@outlook.com>
2021-06-28 13:36:14 -0400

    added throttle decorator in adiciona()

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -24,6 +24,7 @@ export class NegociacaoController {
         this._negociacoesView.update(this._negociacoes);
     }
 
+    @throttle()
     adiciona(event: Event) {
 
         event.preventDefault();
