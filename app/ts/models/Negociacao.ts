b6744d80a65e3386739370e682c81fa3ebfafaad
Luan Uchôa <luan_uchoa@outlook.com>
2021-06-30 12:07:37 -0400

    apply Igualavel interface in code for avoiding duplicated data

--- app/ts/models/Negociacao.ts
+++ app/ts/models/Negociacao.ts
@@ -1,6 +1,7 @@
-import { Imprimivel } from './Imprimivel';
+import { Imprimivel, Igualavel } from './index';
 
-export class Negociacao implements Imprimivel {
+export class Negociacao implements Imprimivel, Igualavel<Negociacao> {
+
     constructor(readonly data: Date, readonly quantidade: number, readonly valor: number) {}
 
     get volume() {
@@ -15,6 +16,13 @@ export class Negociacao implements Imprimivel {
             Quantidade: ${this.quantidade}, 
             Valor: ${this.valor}, 
             Volume: ${this.volume}`
-        );
+        )
+    }
+
+    ehIgual(negociacao: Negociacao): boolean {
+
+        return this.data.getDate() == negociacao.data.getDate()
+            && this.data.getMonth() == negociacao.data.getMonth()
+            && this.data.getFullYear() == negociacao.data.getFullYear();
     }
 }
